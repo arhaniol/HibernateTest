f546a288382b5607ef1a04833a9de589e330bad9
arhaniol <arhaniol@op.pl>
2020-09-13 23:27:41 +0200

    testing class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,29 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        StudentImpl student=new StudentImpl();
-        student.saveStudent(7,"Pola");
-        student.close();
-        System.out.println( "Hello World!" );
+import org.example.jpa.PersonImpl;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        //hibernate
+//        StudentImpl student=new StudentImpl();
+//        student.saveStudent(7,"Pola");
+//        student.close();
+
+        //JPA
+        PersonImpl czlowiek = new PersonImpl();
+//        czlowiek.save(2,"Marek");
+        czlowiek.save("Jozek");
+        czlowiek.save("Marek");
+        System.out.println("czekam...");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Zmieniam imie " + czlowiek.getName(2));
+        czlowiek.update(2, "Jurek");
+        czlowiek.close();
     }
 }
