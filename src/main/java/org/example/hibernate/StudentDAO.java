f1506d2590d5b23b2f197097ff23b185b7c8ac8b
arhaniol <arhaniol@op.pl>
2020-09-13 23:23:22 +0200

    code style

--- src/main/java/org/example/hibernate/StudentDAO.java
+++ src/main/java/org/example/hibernate/StudentDAO.java
@@ -1,10 +1,13 @@
-package org.example;
+package org.example.hibernate;
 
 import java.util.List;
 
 public interface StudentDAO {
     void saveStudent(int id, String name);
+
     Student getStudent(int id);
+
     List getAllStudents();
+
     void updateStudent(int id, String name);
 }
